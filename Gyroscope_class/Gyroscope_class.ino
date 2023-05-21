ae9e7b738f340f4b117102324052a482248f3f7e
Chavero2001 <116393972+Chavero2001@users.noreply.github.com>
2023-05-20 23:09:55 -0400

    Update Gyroscope_class.ino

--- Gyroscope_class/Gyroscope_class.ino
+++ Gyroscope_class/Gyroscope_class.ino
@@ -1,36 +1,36 @@
-#include "gyro.h"
+#include "gyro.h" //Add library
 
-AcGyro Test(9600);
+AcGyro Test(9600); //Create the object and define the velocity
 
 void setup() {
-  // put your setup code here, to run once:
-  Test.initMPU();
+
+  Test.initMPU(); //Initialize MPU
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
- float aX=Test.readAccelerationX();
- float aY=Test.readAccelerationY();
- float aZ=Test.readAccelerationZ();
-   Serial.print("Acceleration X: ");
+  
+ float aX=Test.readAccelerationX(); //Get acceleration of X
+ float aY=Test.readAccelerationY(); //Get acceleration of y
+ float aZ=Test.readAccelerationZ(); //Get acceleration of z
+   Serial.print("Acceleration X: ");    //Print acceleration of X
      Serial.print(aX);
-     Serial.print(" Acceleration Y: ");
+     Serial.print(" Acceleration Y: "); //Print acceleration of y
      Serial.print(aY);
-     Serial.print(" Acceleration Z: ");
+     Serial.print(" Acceleration Z: "); //Print acceleration of z
      Serial.print(aZ);
 
- float gX=Test.readGyroX();
- float gY=Test.readGyroY();
- float gZ=Test.readGyroZ();
-   Serial.print(" Rotation X: ");
+ float gX=Test.readGyroX(); //Get values from the gyroscope on the X axis
+ float gY=Test.readGyroY(); //Get values from the gyroscope on the Y axis
+ float gZ=Test.readGyroZ(); //Get values from the gyroscope on the Z axis
+   Serial.print(" Rotation X: "); //Print values
      Serial.print(gX);
      Serial.print(" Rotation Y: ");
      Serial.print(gY);
      Serial.print(" Rotation Z: ");
      Serial.print(gZ);
 
- float tem=Test.readTemp();
- Serial.print(" Temp: ");
+ float tem=Test.readTemp(); //Get values for temperature 
+ Serial.print(" Temp: "); //Printe Temperature values
      Serial.println(tem); 
      
 }
