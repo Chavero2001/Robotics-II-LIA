1012202fbf8a73a3039c33640d11b153ad5f55fb
Chavero2001 <116393972+Chavero2001@users.noreply.github.com>
2023-05-20 23:13:48 -0400

    Update gyro.h

--- Gyroscope_class/gyro.h
+++ Gyroscope_class/gyro.h
@@ -1,30 +1,31 @@
-#ifndef gyroscope_H
+#ifndef gyroscope_H //Define if not defined
+
 #define gyroscope_H
 
-#include <Arduino.h>
+#include <Arduino.h> //Include arduino library 
 
-#include <Adafruit_MPU6050.h>
-#include <Adafruit_Sensor.h>
-#include <Wire.h>
+#include <Adafruit_MPU6050.h> //Include library for MPU
+#include <Adafruit_Sensor.h> //Include adafruit library
+#include <Wire.h> //Include wire library for serial communication
 
-class AcGyro {
+class AcGyro { //create a class
   private:
-    Adafruit_MPU6050 mpu;
-  int _baud;
+    Adafruit_MPU6050 mpu; //Create an object 
+  int _baud;  //Variable that stores the baud value
 
 
   public:
-    AcGyro(){};
-    AcGyro(int baud);
+    AcGyro(){}; //Default constructor
+    AcGyro(int baud); //Constructor that receives the value of the baud
     
-    void initMPU();  
-    float readAccelerationX();
-    float readAccelerationY();
-    float readAccelerationZ();
-    float readGyroX();
-    float readGyroY();
-    float readGyroZ();
-    float readTemp();
+    void initMPU();   //method to init Sensor
+    float readAccelerationX();  //Method for acceleration in X
+    float readAccelerationY(); //Method for acceleration in Y
+    float readAccelerationZ(); //Method for acceleration in Z
+    float readGyroX(); //Method for rotation in X
+    float readGyroY(); //Method for rotation in Y
+    float readGyroZ(); //Method for rotation in Z
+    float readTemp(); //Method for temperature
 
 };
 
